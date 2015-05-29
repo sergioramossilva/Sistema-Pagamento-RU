32115388ad6c7f269c13054328507e6cb26b7fb9
Felipe V Ramos <fveigaramos@gmail.com>
2015-05-29 14:54:13 -0300

    Correção do nome dos métodos do funcionário que possuiam (dt) para (data).

--- src/main/java/br/edu/utfpr/cm/pi/beans/teste.java
+++ src/main/java/br/edu/utfpr/cm/pi/beans/teste.java
@@ -8,13 +8,22 @@ public class teste {
 
         Cargo cargo1 = new Cargo();
         cargo1.setNome("Cozinheira");
-        cargo1.setAdmin(false);
+        cargo1.setAdmin(true);
         Cargo cargo2 = new Cargo();
 
         CargoDao cargoDao = new CargoDao();
         cargoDao.save(cargo1);
         cargo2 = cargoDao.findById(1L);
         System.out.println(cargo2);
+        cargo1.setNome("atendente");
+        cargoDao.update(cargo1);
+        System.out.println(cargoDao.getAll());
+        
+        Funcionario funcionario1 = new Funcionario();
+        funcionario1.setCargo(cargo1);
+        funcionario1.setCpf("xxxxxxxxxxx");
+        
+        
 
     }
 }
