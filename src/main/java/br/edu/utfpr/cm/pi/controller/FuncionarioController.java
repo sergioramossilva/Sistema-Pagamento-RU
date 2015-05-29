47a40aaeb201d8524989e6593278592f39342873
raphaelutfpr <raphael_lds4630@hotmail.com>
2015-05-28 21:11:25 -0300

    Funcionário Controller

---
+++ src/main/java/br/edu/utfpr/cm/pi/controller/FuncionarioController.java
@@ -0,0 +1,59 @@
+package br.edu.utfpr.cm.pi.controller;
+
+
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import br.edu.utfpr.cm.pi.beans.Funcionario;
+import br.edu.utfpr.cm.pi.daos.FuncionarioDao;
+
+
+/**
+ *
+ * @author raphael
+ */
+@WebServlet(name = "FuncionarioController", urlPatterns = {"/cadastros/funcionario/controlador"})
+public class FuncionarioController extends SuperController{
+
+	   private FuncionarioDao dao;
+	   
+	   public FuncionarioController() {
+		dao = new FuncionarioDao();
+	}	   
+	
+    @Override
+    public String acaoPadrao(HttpServletRequest request) {
+        List funcionarios = dao.getAll();
+        request.setAttribute("lista", funcionarios);
+        return "lista.jsp";
+    }
+    
+    public String incluir(HttpServletRequest request) {
+        Funcionario funcionario = new Funcionario();
+        request.setAttribute("fun", funcionario);
+        return "form.jsp";
+    }
+    
+    public String salvar(HttpServletRequest request) {
+    	Funcionario funcionario = new Funcionario();
+        dao.save(funcionario);
+       return acaoPadrao(request);
+    }
+    
+    public String excluir(HttpServletRequest request){
+    	Funcionario funcionario = new Funcionario();
+        Long id = Long.parseLong(request.getParameter("id"));
+        dao.delete(funcionario.getClass(), id);
+        return acaoPadrao(request);
+    }
+    
+    public String alterar(HttpServletRequest request){
+    	Funcionario funcionario = new Funcionario();
+        Long id = Long.parseLong(request.getParameter("id"));
+        Funcionario fun = (Funcionario) dao.findById(funcionario.getClass(), id);
+        request.setAttribute("car", fun);
+        return "form.jsp";
+    }
+    
+}
+
