ca931c9b67340306033a4fad265952310eee90c6
Felipe V Ramos <fveigaramos@gmail.com>
2015-05-28 22:53:36 -0300

    teste atualizado.

--- src/main/java/br/edu/utfpr/cm/pi/beans/teste.java
+++ src/main/java/br/edu/utfpr/cm/pi/beans/teste.java
@@ -4,20 +4,17 @@ import br.edu.utfpr.cm.pi.daos.CargoDao;
 
 public class teste {
 
-	public static void main(String[] args) {
-		
-		Cargo cargo = new Cargo();
-		cargo.setNome("Cozinheira");
-		cargo.setAdmin(false);
-		
-		CargoDao dao = new CargoDao();
-		
-		System.out.println("Salvando... ");
-		dao.save(cargo);
-		
-		
-		
-		
-	}
+    public static void main(String[] args) {
 
+        Cargo cargo1 = new Cargo();
+        cargo1.setNome("Cozinheira");
+        cargo1.setAdmin(false);
+        Cargo cargo2 = new Cargo();
+
+        CargoDao cargoDao = new CargoDao();
+        cargoDao.save(cargo1);
+        cargo2 = cargoDao.findById(1L);
+        System.out.println(cargo2);
+
+    }
 }
