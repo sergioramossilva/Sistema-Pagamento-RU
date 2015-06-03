4e56b95b2c2257a0ff03a40803e2c8ffae9f9413
sergioramossilva <sergiormsilva@yahoo.com>
2015-06-03 19:55:50 -0300

    Adição da biblioteca JspRuntimeLibrary; Alteração do método salvar na classe CargoController

--- src/main/java/br/edu/utfpr/cm/pi/controller/CargoController.java
+++ src/main/java/br/edu/utfpr/cm/pi/controller/CargoController.java
@@ -1,7 +1,5 @@
 package br.edu.utfpr.cm.pi.controller;
 
-
-
 import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
@@ -13,61 +11,64 @@ import org.apache.jasper.runtime.JspRuntimeLibrary;
 import br.edu.utfpr.cm.pi.beans.Cargo;
 import br.edu.utfpr.cm.pi.daos.CargoDao;
 
-
 /**
  *
  * @author raphael
  * 
- * ppp
+ *         ppp
  */
-@WebServlet(name = "CargoController", urlPatterns = {"/cadastros/cargo/controlador"})
-public class CargoController extends SuperController{
+@WebServlet(name = "CargoController", urlPatterns = { "/cadastros/cargo/controlador" })
+public class CargoController extends SuperController {
 
-	   /**
+    /**
      * 
      */
     private static final long serialVersionUID = -2370921254016052104L;
     private CargoDao dao;
-	   
-	   public CargoController() {
-		dao = new CargoDao();
-	}	   
-	
+
+    public CargoController() {
+        dao = new CargoDao();
+    }
+
     @Override
     public String acaoPadrao(HttpServletRequest request) {
         List<Cargo> cargos = dao.getAll();
         request.setAttribute("lista", cargos);
         return "lista.jsp";
     }
-    
+
     public String incluir(HttpServletRequest request) {
         Cargo cargo = new Cargo();
         request.setAttribute("cargo", cargo);
         return "FormCargo.jsp";
     }
-    
+
     public String salvar(HttpServletRequest request) throws JasperException {
         Cargo cargo = new Cargo();
         JspRuntimeLibrary.introspect(cargo, request);
-        dao.save(cargo);
-       return acaoPadrao(request);
+
+        if (cargo.getId() == null) {
+            dao.save(cargo);
+        } else {
+            dao.update(cargo);
+        }
+        return acaoPadrao(request);
     }
-    
-    public String excluir(HttpServletRequest request){
+
+    public String excluir(HttpServletRequest request) {
         Cargo cargo = new Cargo();
         Long id = Long.parseLong(request.getParameter("id"));
         cargo.setId(id);
         dao.delete(cargo);
         return acaoPadrao(request);
     }
-    
-    public String alterar(HttpServletRequest request){
+
+    public String alterar(HttpServletRequest request) {
         Cargo cargo = new Cargo();
         Long id = Long.parseLong(request.getParameter("id"));
         cargo = dao.findById(id);
         request.setAttribute("cargo", cargo);
         return "FormCargo.jsp";
     }
-    
-}
 
+}
