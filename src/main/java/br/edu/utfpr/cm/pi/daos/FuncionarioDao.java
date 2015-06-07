f7e7e3a32b25d98582f60fa7b8b2bf3f219560e6
sergioramossilva <sergiormsilva@yahoo.com>
2015-06-07 15:40:57 -0300

    Servlet de Login modificado

--- src/main/java/br/edu/utfpr/cm/pi/daos/FuncionarioDao.java
+++ src/main/java/br/edu/utfpr/cm/pi/daos/FuncionarioDao.java
@@ -1,11 +1,37 @@
 package br.edu.utfpr.cm.pi.daos;
 
+import javax.persistence.Query;
+
 import br.edu.utfpr.cm.pi.beans.Funcionario;
+import br.edu.utfpr.cm.pi.persistence.PersistenceManager;
 
 public class FuncionarioDao extends JpaDao<Funcionario, Long> {
 
     public FuncionarioDao() {
         super(Funcionario.class);
+    }
+
+    public Funcionario getLogin(String login, String senha) {
+        Funcionario funcionario = null;
+
+        try {
+
+            manager = PersistenceManager.getEntityManager();
+            Query query = manager
+                    .createQuery("SELECT f FROM funcionario as f WHERE f.login = :login AND f.senha = :senha");
+            query.setParameter("login", login);
+            query.setParameter("senha", senha);
+
+            funcionario = (Funcionario) query.getSingleResult();
+        } catch (Exception ex) {
+
+            ex.getMessage();
+        } finally {
+
+            manager.close();
+        }
+
+        return funcionario;
     }
 
 }
