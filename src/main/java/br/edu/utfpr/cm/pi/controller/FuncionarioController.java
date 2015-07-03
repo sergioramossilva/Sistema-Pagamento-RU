bb624517bfbd97da73ae68ad275c8568021e3324
sergioramossilva <sergiormsilva@yahoo.com>
2015-07-03 19:50:43 -0300

    Forms mudados para a pasta correta

--- src/main/java/br/edu/utfpr/cm/pi/controller/FuncionarioController.java
+++ src/main/java/br/edu/utfpr/cm/pi/controller/FuncionarioController.java
@@ -1,11 +1,11 @@
-
 package br.edu.utfpr.cm.pi.controller;
 
 import java.util.List;
-import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import br.edu.utfpr.cm.pi.beans.Funcionario;
 import br.edu.utfpr.cm.pi.daos.FuncionarioDao;
 
@@ -17,12 +17,10 @@ public class FuncionarioController {
     public FuncionarioController() {
         fdao = new FuncionarioDao();
     }
-
+    
     @RequestMapping("incluirFuncionario")
-    public String incluir(HttpServletRequest request) {
-        Funcionario funcionario = new Funcionario();
-        request.setAttribute("funcionario", funcionario);
-        return "cadastros/formFuncionario.jsp";
+    public String incluir() {
+        return "cadastros/formFuncionario";
     }
 
     @RequestMapping("salvarFuncionario")
@@ -36,18 +34,18 @@ public class FuncionarioController {
         fdao.delete(funcionario);
         return "forward:listarFuncionarios";
     }
-    
+
     @RequestMapping("alterarFuncionario")
-    public String alterar(Long id, Model model) {        
+    public String alterar(Long id, Model model) {
         model.addAttribute("funcionario", fdao.findById(id));
-        return "cadastros/formFuncionario.jsp";
+        return "cadastros/formFuncionario";
     }
 
     @RequestMapping("listarFuncionarios")
     public String lista(Model model) {
         List<Funcionario> funcionarios = fdao.getAll();
         model.addAttribute("funcionarios", funcionarios);
-        return "listas/listaFuncionarios.jsp";
+        return "listas/listaFuncionarios";
     }
 
 }
