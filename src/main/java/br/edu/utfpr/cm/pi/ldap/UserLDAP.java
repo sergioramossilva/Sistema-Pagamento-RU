3ae418081be485cadb0a5e156321d963629c9d78
Felipe V Ramos <fveigaramos@gmail.com>
2015-07-02 22:54:15 -0300

    Correções efetuadas no LDAP, classes acrescentadas e pon corrigido

---
+++ src/main/java/br/edu/utfpr/cm/pi/ldap/UserLDAP.java
@@ -0,0 +1,19 @@
+package br.edu.utfpr.cm.pi.ldap;
+
+
+import java.io.Serializable;
+
+import br.edu.utfpr.cm.pi.beans.Usuario;
+
+/**
+ *
+ * @author Paulo Azevedo
+ */
+public class UserLDAP extends Usuario implements Serializable{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4482843544558928805L;
+    
+}
