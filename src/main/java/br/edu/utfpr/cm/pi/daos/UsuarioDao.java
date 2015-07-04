540f5314267f37d7f146f1f405ee90a356328e85
Felipe V Ramos <fveigaramos@gmail.com>
2015-07-03 22:39:53 -0300

    alterações itnernas dos daos e ldap

--- src/main/java/br/edu/utfpr/cm/pi/daos/UsuarioDao.java
+++ src/main/java/br/edu/utfpr/cm/pi/daos/UsuarioDao.java
@@ -1,11 +1,33 @@
 package br.edu.utfpr.cm.pi.daos;
 
+import org.hibernate.Query;
+
 import br.edu.utfpr.cm.pi.beans.UsuarioSistema;
+import br.edu.utfpr.cm.pi.persistence.PersistenceManager;
+import br.edu.utfpr.cm.saa.entidades.Usuario;
 
 public class UsuarioDao extends JpaDao<UsuarioSistema, Long> {
 
     public UsuarioDao() {
         super(UsuarioSistema.class);
+    }
+
+    public UsuarioSistema retornaUsuarioDoSistemaAPartirDeUsuarioDoLdap(
+            Usuario ul) {
+        try {
+            manager = PersistenceManager.getEntityManager();
+            Query query = manager.createQuery(
+                    "select u from UsuarioSistema u where login = :login",
+                    UsuarioSistema.class);
+            query.setParameter("login", ul.getLogin());
+
+            return (UsuarioSistema) query.getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            manager.close();
+        }
+        return null;
     }
 
 }
